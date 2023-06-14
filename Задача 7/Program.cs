83cd378e893382fbd9501236a458a907e07b2017
@Aidar Abitov <aydar099@gmail.com>
2023-06-14 16:14:03 +0300

    три ДЗ

---
+++ Задача 7/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Программа, которая принимает на вход 
+трехзначное число и выдает последнюю цифру этого числа */
+
+Console.Clear() ;
+Console.WriteLine("Введите трехзначное число") ;
+int number = int.Parse(Console.ReadLine()) ;
+Console.WriteLine(number%10) ;
